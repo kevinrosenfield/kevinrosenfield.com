efceef84d8410de0a8a7bb7b844d1bf3c0c4bcb0
Kevin Rosenfield <kevin.rosenfield@gmail.com>
2023-08-05 13:55:56 -0400

    edit js

--- functions.js
+++ functions.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
             console.log(child)
             // For example, set the background color of each child to red
             child.style.height = '55px';
+            child.innerHTML = 'TEXT HERE';
         });
     });
     
@@ -25,6 +26,7 @@ $(document).ready(function () {
         const childrenOfHoveredElement = div.querySelectorAll(':not(img)');
         childrenOfHoveredElement.forEach((child) => {
             child.style.height = 0; // Remove the background color
+            child.innerHTML = 'TEXT HERE';
         });
     });
 }); 
